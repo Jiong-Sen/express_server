fd793eeb2d265773d290e0676ee927c48118406d
wangkai <wangkai@smm.cn>
2022-02-04 17:43:42 +0800

    fix: 更改为参数模式

--- controller/screenshot.js
+++ controller/screenshot.js
@@ -1,15 +1,18 @@
 const puppeteer = require('puppeteer')
 
-exports.screenshot = (req, res, next) => {
+exports.screenshot = async (req, res, next) => {
   try {
+    console.log(req.query.url, 'req.params')
+    const hosts = req.query.url.split('www.')[1].split('/')[0]
+
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto('https://baidu.com')
-    await page.screenshot({ path: 'example.png' })
+    await page.goto('https://www.baidu.com')
+    await page.screenshot({ path: `../screenshot/${hosts}.png` })
 
     await browser.close()
 
-    res.send('screenshot web image')
+    res.send('screenshot web success')
   } catch (error) {
     next(err)
   }
--- router/article.js
+++ router/article.js
@@ -1,37 +1,37 @@
-// const express = require('express')
-// const router = express.Router()
+const express = require('express')
+const router = express.Router()
 
-// router.post('/', (req, res, next) => {
-//   try {
-//     res.send('get user')
-//   } catch (error) {
-//     next(err)
-//   }
-// })
-// // 获取w文章列表
-// router.get('/', (req, res, next) => {
-//   try {
-//     res.send('get  /')
-//   } catch (error) {
-//     next(err)
-//   }
-// })
+router.post('/', (req, res, next) => {
+  try {
+    res.send('get user')
+  } catch (error) {
+    next(err)
+  }
+})
+// 获取w文章列表
+router.get('/', (req, res, next) => {
+  try {
+    res.send('get  /')
+  } catch (error) {
+    next(err)
+  }
+})
 
-// //
-// router.get('/feed', (req, res, next) => {
-//   try {
-//     res.send('post :username')
-//   } catch (error) {
-//     next(err)
-//   }
-// })
-// // 取消关注
-// router.get('/:slug', (req, res, next) => {
-//   try {
-//     res.send('get user')
-//   } catch (error) {
-//     next(err)
-//   }
-// })
+//
+router.get('/feed', (req, res, next) => {
+  try {
+    res.send('post :username')
+  } catch (error) {
+    next(err)
+  }
+})
+// 取消关注
+router.get('/:slug', (req, res, next) => {
+  try {
+    res.send('get user')
+  } catch (error) {
+    next(err)
+  }
+})
 
-// module.exports = router
+module.exports = router
